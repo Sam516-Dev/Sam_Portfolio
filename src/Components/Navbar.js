b5dbbffa2338c6c17caa0e2eaa397abdf1c0c9c9
Sam <samuelnjoroge3455@gmail.com>
2023-08-20 21:04:42 +0300

    packages added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import samlogo from "../assets/samlogo.PNG";
+// import samlogo from "../assets/samlogo.PNG";
 
 const Navbar = () => {
   // Add state to handle the mobile menu
